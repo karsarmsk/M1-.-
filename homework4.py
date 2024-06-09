bd4a46dec769c17b57c16eac4b4920ed9e66965e
Сергей Карасев <karasev-s@internet.ru>
2024-06-09 13:47:55 +0300

    Add files via upload

---
+++ homework4.py
@@ -0,0 +1,7 @@
+my_string = input('Введите название города ') # Создал переменную
+print(len(my_string)) # Вывел длину символов введённого текста
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(' ',''))# меняю строку my_string, удалив все пробелы
+print(my_string[0])
+print(my_string[-1])
